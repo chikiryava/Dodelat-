3dda4f82e030d06ca57967482b5c2f2237cb2e28
chikiryava <98350681+chikiryava@users.noreply.github.com>
2023-02-08 11:42:27 +0500

    Add files via upload

---
+++ 08.02/08.02/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08._02
+{
+    public class Student
+    {
+        public string name;
+        public int rost = 165;
+        static double ves = 65;
+        public string GetName()
+        {
+            
+            return name;
+        }
+        public double GetEat()
+        {
+            return ves;
+        }
+        public double SetEat(double eda,double weight)
+        {
+            weight += eda - 1.6;
+            return weight;
+        } 
+    }
+}
---
+++ 08.02/08.02/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _08._02
+{
+    public partial class Form1 : Form
+    {
+        Student student = new Student();
+        public Form1()
+        {
+            InitializeComponent();
+            textBox4.Text = student.GetEat().ToString();
+            numericUpDown1.Value = student.rost;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            textBox4.Text = student.SetEat(double.Parse(textBox3.Text), double.Parse(textBox4.Text)).ToString();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
